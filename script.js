675ab8d7d868a6e597c7e4ab6f1fe1738da7179b
jisha66 <mydreamplace2020@gmail.com>
2022-05-17 12:48:19 +0600

    frst page

---
+++ script.js
@@ -0,0 +1,8 @@
+// some problem issu
+// let searchBtn = document.querySelector('#search-btn');
+// let searchBar = document.querySelector('search-bar-container');
+
+// searchBtn.addEventListener('click', () => {
+//     searchBtn.classList.toggle('fa-times');
+//     searchBar.classList.toggle('active');
+// });
